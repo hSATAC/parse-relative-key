94d134b1b80a055d83b85a852edd55d32d03126b
hSATAC <hsatac@gmail.com>
2014-04-11 15:17:24 +0800

    Implement command line

--- parse.rb
+++ parse.rb
@@ -1,10 +1,6 @@
 #!/usr/bin/env ruby
 require 'parser/current'
 
-folder_path = ARGV[0]
-
-# == class ==
-
 class Parse
   attr_reader :filename, :code, :ast
   def initialize(filename)
@@ -22,7 +18,10 @@ class Parse
   end
 
   def find_relative_key_on_ast(ast)
-    return recursive_find_relative_key_on_ast(ast, [])
+    method_name = ast.to_a[0]
+    result = recursive_find_relative_key_on_ast(ast, [])
+    result.each { |hash| hash[:method] = method_name }
+    return result
   end
 
   private
@@ -51,7 +50,7 @@ class Parse
 
   def recursive_find_relative_key_on_ast(ast, result)
     if ast.type == :send && ast.to_a[1] == :t && ast.to_a[2].to_a[0].start_with?('.')
-      result << { :line => ast.loc.line, :key => ast.to_a[2].to_a[0] }
+      result << { :line => ast.loc.line, :key => ast.to_a[2].to_a[0], :file => @filename }
     else
       for child in ast.children
         recursive_find_relative_key_on_ast(child, result) if child && child.instance_of?(Parser::AST::Node)
@@ -61,3 +60,25 @@ class Parse
     return result
   end
 end
+
+# === command line ===
+#
+folder_path = ARGV[0]
+files = Dir[File.join(folder_path, '**', '*.rb')]
+
+# get all before_actions
+all_before_actions = files.map { |file| Parse.new(file).find_before_actions }.flatten.uniq.compact
+
+# find all i18n relative key in these before_actions
+all_suspect = files.map do |file|
+  p = Parse.new(file)
+  matched_methods = all_before_actions.map { |method| p.find_method method }.compact
+  result = matched_methods.map do |ast|
+    found = p.find_relative_key_on_ast(ast)
+    found.empty? ? nil : found
+  end.compact
+  result.empty? ? nil : result
+end.compact.flatten
+
+# Print the outpt
+all_suspect.each { |hash| puts "#{hash[:file]}:#{hash[:line]} ##{hash[:method]} - #{hash[:key]}" }
--- spec/parse_spec.rb
+++ spec/parse_spec.rb
@@ -13,12 +13,17 @@ RSpec.describe Parse do
     expect(p.find_before_actions).to eq([:find_organiztion, :grant_permissions_for_organization, :check_business_info_type, :get_order])
   end
 
-  it 'find_method' do
-    expect(p.find_method(:find_organiztion).loc.keyword.line).to eq(44)
+  context 'find_method' do
+    it 'find the correct ast' do
+      expect(p.find_method(:find_organiztion).loc.keyword.line).to eq(44)
+    end
+    it 'return nil when not found' do
+      expect(p.find_method(:no_method)).to be(nil)
+    end
   end
 
   it 'find_relative_key_on_ast' do
     ast = p.find_method(:check_business_info_type)
-    expect(p.find_relative_key_on_ast(ast)).to eq([{ :line => 50, :key => '.plase_fill_in_info_for_invoice_generation' }])
+    expect(p.find_relative_key_on_ast(ast)).to eq([{ :line => 50, :key => '.plase_fill_in_info_for_invoice_generation', :file => FILE, :method => :check_business_info_type }])
   end
 end
